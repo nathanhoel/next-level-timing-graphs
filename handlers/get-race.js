0d92b0eedefec080be4e525d50678bbf96e14549
Nathan Hoel <nathanhoel@gmail.com>
2020-03-16 00:02:51 -0400

    rank laps

--- handlers/get-race.js
+++ handlers/get-race.js
@@ -13,7 +13,11 @@ module.exports.handle = async function (event, context, callback) {
   };
 
   const result = await dynamoDb.query(params).promise();
-  const results = result.Items;
+  const allResults = result.Items;
+
+  const bestResults = _getDriverBestResults(allResults);
+  _accumalteLaps(bestResults);
+  _rankLaps(bestResults);
 
   const html = `
   <html>
@@ -22,12 +26,12 @@ module.exports.handle = async function (event, context, callback) {
       <script src="//cdn.jsdelivr.net/chartist.js/latest/chartist.min.js"></script>
     </head>
     <body>
+      ${JSON.stringify(bestResults)}
       <div style="width: 80%;">
         <div class="ct-chart"></div>
       </div>
       <script>
         var chart = new Chartist.Line('.ct-chart', {
-
           series: [
             [{x: 1, y: 1}, {x:1.24, y: 1}, {x: 2.6, y:1}, {x:3.9, y: 2}, {x:5.6, y: 2}, {x:6.2, y: 1}],
             [{x: 1, y: 2}, {x:1.54, y: 2}, {x: 2.71, y:2}, {x:3.77, y: 1}, {x:5.23, y: 1}, {x:6.24, y: 2}],
@@ -75,3 +79,47 @@ module.exports.handle = async function (event, context, callback) {
 
   callback(null, response);
 };
+
+function _getDriverBestResults(results) {
+  const bestResults = [];
+  for (let i = 0; i < results.length; i++) {
+    (i === 0 || results[i].name !== results[i - 1].name) && bestResults.push(results[i]);
+  }
+  return bestResults;
+}
+
+function _accumalteLaps(results) {
+  for (const result of results) {
+    const accumaltedLaps = [];
+    for (let i = 0; i < result.laps.length; i++) {
+      accumaltedLaps[i] = (accumaltedLaps[i - 1] || 0) + result.laps[i];
+    }
+    result.accumaltedLaps = accumaltedLaps;
+  }
+}
+
+function _rankLaps(results) {
+  const maxLaps = Math.max.apply(Math, array.map(result => result.laps.length));
+  for (const result of results) {
+    result.rankedLaps = [];
+  }
+
+  for (let i = 0; i < maxLaps; i++) {
+    const thisLapForAllDrivers = [];
+    for (const result of results) {
+      const time = result.accumaltedLaps[i];
+      if (!time) {
+        continue;
+      }
+
+      const rankedLap = { time };
+      result.rankedLaps.push(rankedLap);
+      thisLapForAllDrivers.push(rankedLap);
+    }
+
+    thisLapForAllDrivers.sort((a, b) => a.time - b.time);
+    for (let i = 0; i < thisLapForAllDrivers.length; i++) {
+      thisLapForAllDrivers[i].position = i + 1;
+    }
+  }
+}

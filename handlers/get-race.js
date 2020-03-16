fcae46ad7a9b78a77e49ffe4ead417b4c9e4768b
Nathan Hoel <nathanhoel@gmail.com>
2020-03-15 22:47:05 -0400

    chart changes

--- handlers/get-race.js
+++ handlers/get-race.js
@@ -22,27 +22,43 @@ module.exports.handle = async function (event, context, callback) {
       <script src="//cdn.jsdelivr.net/chartist.js/latest/chartist.min.js"></script>
     </head>
     <body>
-      <div class="ct-chart ct-perfect-fourth"></div>
+      <div class="ct-chart"></div>
       ${JSON.stringify(results)}
 
       <script>
         var chart = new Chartist.Line('.ct-chart', {
-          labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
+
           series: [
-            [5, 5, 10, 8, 7, 5, 4, null, null, null, 10, 10, 7, 8, 6, 9],
-            [10, 15, null, 12, null, 10, 12, 15, null, null, 12, null, 14, null, null, null],
-            [null, null, null, null, 3, 4, 1, 3, 4,  6,  7,  9, 5, null, null, null],
-            [{x:3, y: 3},{x: 4, y: 3}, {x: 5, y: undefined}, {x: 6, y: 4}, {x: 7, y: null}, {x: 8, y: 4}, {x: 9, y: 4}]
+            [{x: 1, y: 1}, {x:1.24, y: 1}, {x: 2.6, y:1}, {x:3.9, y: 2}, {x:5.6, y: 2}, {x:6.2, y: 1}],
+            [{x: 1, y: 2}, {x:1.54, y: 2}, {x: 2.71, y:2}, {x:3.77, y: 1}, {x:5.23, y: 1}, {x:6.24, y: 2}],
+            [{x: 1, y: 3}, {x:1.54, y: 2}, {x: 2.71, y:2}, {x:3.77, y: 1}, {x:5.23, y: 1}, {x:6.24, y: 2}],
           ]
         }, {
           fullWidth: true,
+          lineSmooth: false,
+          onlyInteger: false,
           chartPadding: {
             right: 10
           },
-          lineSmooth: Chartist.Interpolation.cardinal({
-            fillHoles: true,
-          }),
-          low: 0
+          low: 1,
+          axisX: {
+            type: Chartist.FixedScaleAxis,
+            ticks: [1,2,3,4,5,6,7,8,9],
+            onlyInteger: true
+          },
+          axisY: {
+            onlyInteger: true,
+            showGrid: true,
+            low: 0,
+            showLabel: true,
+            divisor: 3
+          },
+          chartPadding: {
+            top: 10,
+            right: 90,
+            bottom: 0,
+            left: 50
+          },
         });
       </script>
     </body>

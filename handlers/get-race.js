022d842a3e0ba55fe55e3cb960261506cb925553
Nathan Hoel <nathanhoel@gmail.com>
2020-03-16 23:58:26 -0400

    fix css

--- handlers/get-race.js
+++ handlers/get-race.js
@@ -116,7 +116,8 @@ module.exports.handle = async function (event, context, callback) {
         }
 
         .fastest-lap {
-          color:
+          color: #874ff4;
+          font-weight: 600;
         }
       </style>
     </head>
@@ -366,7 +367,7 @@ function _msToTimeFormat(ms, displayMinutes = false) {
   const minutesLabel = Math.floor(ms / 60000).toString().padStart(2, '0');
   const leftoverSeconds = displayMinutes ? ms % 60000 : ms;
   const secondsLabel = Math.floor(leftoverSeconds / 1000).toString().padStart(2, '0');
-  const msLabel = (ms % 1000).toString().padEnd(3, '0');
+  const msLabel = (ms % 1000).toString().padStart(3, '0');
   const label = minutesLabel + ':' + secondsLabel + '.' + msLabel;
   return displayMinutes ? label : label.split(':')[1];
 }
